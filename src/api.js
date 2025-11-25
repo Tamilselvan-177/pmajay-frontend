e22dea82b043bfc25be2fd5e3269031efb8ed3ea
Tamilselvan-177 <231801177@rajalakshmi.edu.in>
2025-11-25 09:45:44 +0530

    okay

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://walking-format-predictions-aids.trycloudflare.com/",
+  baseURL: "https://backendpmajay.onrender.com/",
 });
 
 // 🔥 Automatically attach token to all requests
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://walking-format-predictions-aids.trycloudflare.com/',
+        target: 'https://backendpmajay.onrender.com/',
         changeOrigin: true,
         secure: false,
       },
