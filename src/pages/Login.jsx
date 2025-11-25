20a706cc5bf9bcd62c7270e29f710fd27b4dcde6
Tamilselvan-177 <231801177@rajalakshmi.edu.in>
2025-11-25 10:03:19 +0530

    hosting in render

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
-  const API_BASE = 'http://127.0.0.1:5000/api';
+  const API_BASE = 'https://backendpmajay.onrender.com/api';
 
   useEffect(() => {
     if (!isLogin) {
